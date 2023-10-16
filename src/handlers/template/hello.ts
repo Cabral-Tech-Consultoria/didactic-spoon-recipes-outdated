765e23f56ce245cc5706c5c23abbecf10f7b1278
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-16 14:13:58 -0300

    chore: start the base project

---
+++ src/handlers/template/hello.ts
@@ -0,0 +1,8 @@
+import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda'
+
+export const handle = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ message: `Hello, ${event.queryStringParameters?.name}` })
+	}
+}
