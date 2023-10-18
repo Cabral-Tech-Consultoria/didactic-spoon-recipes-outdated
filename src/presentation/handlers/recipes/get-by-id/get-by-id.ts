46d86f26af4da31258612b4413685020c27ab223
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-18 15:31:00 -0300

    feat: create a get recipe by id handler

---
+++ src/presentation/handlers/recipes/get-by-id/get-by-id.ts
@@ -0,0 +1,9 @@
+import {APIGatewayProxyEvent, APIGatewayProxyResult, Handler} from 'aws-lambda'
+import {getById} from '../../../controllers/recipes/get-by-id'
+
+export const handle: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+	return getById(
+		Number(event.pathParameters?.id),
+		event.queryStringParameters?.includeNutrition === 'true'
+	)
+}
