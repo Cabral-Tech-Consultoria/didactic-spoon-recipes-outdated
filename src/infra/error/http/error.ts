59c7eb863ad95f2fa534e8c5744d6dfa9aa9d279
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-18 09:48:56 -0300

    feat: create a method missing param error

--- src/infra/error/http/error.ts
+++ src/infra/error/http/error.ts
@@ -1,9 +1,18 @@
 import {InternalServerError} from '../internal-server.error'
+import {MissingParamError} from '../missing-param.error'
 
 export const internalServerError = () => {
 	const err = new InternalServerError()
 	return {
 		statusCode: 500,
+		body: JSON.stringify({ message: err.message })
+	}
+}
+
+export const missingParamError = (param: string) => {
+	const err = new MissingParamError(param)
+	return {
+		statusCode: 406,
 		body: JSON.stringify({ message: err.message })
 	}
 }
