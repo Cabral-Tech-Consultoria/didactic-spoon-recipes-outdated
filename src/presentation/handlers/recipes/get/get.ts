0b24cc38fb908d9b4556aefde626becf68858d93
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-18 15:28:10 -0300

    chore: add typing to handle function

--- src/presentation/handlers/recipes/get/get.ts
+++ src/presentation/handlers/recipes/get/get.ts
@@ -1,8 +1,8 @@
-import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda'
+import {APIGatewayProxyEvent, APIGatewayProxyResult, Handler} from 'aws-lambda'
 import {ConvertTo} from '../../../../utils/convertion/converter'
 import {BaseQueryStringSearch} from '../../../../infra/protocols/interfaces/querystring.interface'
 import {get} from '../../../controllers/recipes/get'
 
-export const handle = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handle: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 	return get(ConvertTo<BaseQueryStringSearch>(event.queryStringParameters))
 }
