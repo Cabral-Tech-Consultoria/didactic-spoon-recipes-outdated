4af857d6e08e70763e91514587be048ae5950fcd
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-18 17:02:18 -0300

    chore: create an object helper to validate if all props are empty

---
+++ src/utils/objects/check-null-properties.ts
@@ -0,0 +1,15 @@
+const getProperty = <T, K extends keyof T>(object: T, keyName: K): T[K] =>  {
+	return object[keyName]
+}
+
+export const IsObjectEmpty = (item: object) => {
+	let isEmpty = true
+
+	Object.keys(item).forEach(key => {
+		if (getProperty(item, key as keyof object)) {
+			isEmpty = false
+		}
+	})
+
+	return isEmpty
+}
