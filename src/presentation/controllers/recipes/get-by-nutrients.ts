9f9ba2288a0796e3ecf398f549d1c21498bd9a5b
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-19 05:49:40 -0300

    feat: add logs and validation

--- src/presentation/controllers/recipes/get-by-nutrients.ts
+++ src/presentation/controllers/recipes/get-by-nutrients.ts
@@ -4,19 +4,29 @@ import RecipeService, {noContent, ok} from '../../../infra/http'
 import {INutrients} from '../../../domain/protocols/interfaces/nutrients.interface'
 import {IsObjectEmpty} from '../../../utils/objects/check-null-properties'
 import {IRecipe} from '../../../domain/protocols/interfaces/recipe.interface'
+import logger from '../../../infra/logger/log'
 
 export const getByNutrients = async (nutrients: INutrients): Promise<APIGatewayProxyResult> => {
 	try {
+		logger.info({ message: 'Params obtained from client', body: nutrients })
 		const url: string = '/findByNutrients'
+
+		logger.info({ message: 'Verify if all properties are empty' })
 		if (IsObjectEmpty(nutrients)) {
+			const params = { ...nutrients, random: true }
+
+			logger.info({ message: 'Search for recipes based on these params', body: params })
+
 			const { data } = await RecipeService
 				.http
-				.get(url, {
-					params: { ...nutrients, random: true }
-				})
+				.get(url, {params})
+
+			logger.info({ message: 'Recipes obtained from api', body: data })
 
 			return ok(data)
 		}
+
+		logger.info({ message: 'Search for recipes based on these params', body: nutrients })
 
 		const { data } = await RecipeService
 			.http
@@ -24,9 +34,15 @@ export const getByNutrients = async (nutrients: INutrients): Promise<APIGatewayP
 				params: { ...nutrients }
 			})
 
-		if (data.length) {
+		logger.info({ message: 'Recipes obtained from api', body: data })
+
+		if (!data.length) {
+			logger.info({ message: 'Return no content if none is found' })
+
 			return noContent()
 		}
+
+		logger.info({ message: 'Recipes obtained from api', body: data })
 
 		return ok(data)
 	} catch {
