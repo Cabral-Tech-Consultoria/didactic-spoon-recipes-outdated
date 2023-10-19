f221adc857220900815f613e5952478da02012e5
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-19 19:10:10 -0300

    feat: create dependency injection class

---
+++ src/infra/dependency-injection/types.di.ts
@@ -0,0 +1,15 @@
+import {Container, interfaces} from 'inversify'
+import ServiceIdentifier = interfaces.ServiceIdentifier;
+import Newable = interfaces.Newable;
+
+export const TYPES_DI = {
+	RecipeService: Symbol.for('RecipeService')
+}
+
+export class DIContainerConfig {
+	static container: Container = new Container()
+
+	static bindClass<T>(type: ServiceIdentifier<T>) {
+		this.container.bind<T>(type).to(type as Newable<T>)
+	}
+}
