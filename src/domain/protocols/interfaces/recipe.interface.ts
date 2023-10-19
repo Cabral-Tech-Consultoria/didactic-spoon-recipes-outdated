f23b53e17150069113a9d363d442067c3062971e
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-19 08:49:45 -0300

    chore: add an interface to similar recipes

--- src/domain/protocols/interfaces/recipe.interface.ts
+++ src/domain/protocols/interfaces/recipe.interface.ts
@@ -1,6 +1,17 @@
+import {ImageType} from './recipe-info.interface'
+
 export interface IRecipe {
   id: number,
   title: string,
   image: string,
-  imageType: 'png' | 'jpg',
+  imageType: ImageType,
+}
+
+export interface ISimilarRecipe {
+  id: number,
+  title: string,
+  imageType: ImageType,
+  readyInMinutes: number,
+  servings: number,
+  sourceUrl: string
 }
