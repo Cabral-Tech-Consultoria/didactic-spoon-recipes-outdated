2fbe341216487f823907b25e81f7ab5995865929
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-20 07:45:11 -0300

    refactor: create a separated constant to hold the search mock

--- test/test-domains/ingredients/index.ts
+++ test/test-domains/ingredients/index.ts
@@ -20,26 +20,28 @@ export const makeSut = () => {
 export const buildIngredientsService = () => {
 	class IngredientsServiceStub implements IIngredientService {
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		search(params?: QueryIngredientSearch): Promise<AxiosResponse<IIngredientSearchList>> {
-			return Promise.resolve(buildAxiosResponse({
-				results: [
-					{
-						'id': 19400,
-						'name': 'banana chips',
-						'image': 'banana-chips.jpg'
-					},
-					{
-						'id': 93779,
-						'name': 'banana liqueur',
-						'image': 'limoncello.jpg'
-					}
-				],
-				offset: 0,
-				number: 0,
-				totalResults: 0
-			}))
+		async search(params?: QueryIngredientSearch): Promise<AxiosResponse<IIngredientSearchList>> {
+			return Promise.resolve(buildAxiosResponse(mockSearch))
 		}
 	}
 
 	return new IngredientsServiceStub()
+}
+
+const mockSearch = {
+	results: [
+		{
+			'id': 19400,
+			'name': 'banana chips',
+			'image': 'banana-chips.jpg'
+		},
+		{
+			'id': 93779,
+			'name': 'banana liqueur',
+			'image': 'limoncello.jpg'
+		}
+	],
+	offset: 0,
+	number: 0,
+	totalResults: 0
 }
