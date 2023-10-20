26eb6182673698ecb9faafc6a0fd6d27e814af85
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-20 07:29:31 -0300

    fix: turn to most of the properties to optional

--- src/infra/protocols/interfaces/query-ingredient-search.interface.ts
+++ src/infra/protocols/interfaces/query-ingredient-search.interface.ts
@@ -1,17 +1,17 @@
 export interface QueryIngredientSearch {
   query: string
-  addChildren: boolean
-  minProteinPercent: number
-  maxProteinPercent: number
-  minFatPercent: number
-  maxFatPercent: number
-  minCarbsPercent: number
-  maxCarbsPercent: number
-  metaInformation: boolean
-  intolerances: string
-  sort: string
-  sortDirection: string
-  language: string
-  offset: number
-  number: number
+  addChildren?: boolean
+  minProteinPercent?: number
+  maxProteinPercent?: number
+  minFatPercent?: number
+  maxFatPercent?: number
+  minCarbsPercent?: number
+  maxCarbsPercent?: number
+  metaInformation?: boolean
+  intolerances?: string
+  sort?: string
+  sortDirection?: string
+  language?: string
+  offset?: number
+  number?: number
 }
