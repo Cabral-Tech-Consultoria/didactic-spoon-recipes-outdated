463021be13687d08ed5bdc6cf56f7f88a6aa5982
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-20 07:43:54 -0300

    chore: implements the no content logic

--- src/presentation/controllers/ingredients/ingredient.controller.ts
+++ src/presentation/controllers/ingredients/ingredient.controller.ts
@@ -2,13 +2,19 @@ import {IIngredientController} from './protocols/i-ingredient.controller'
 import {QueryIngredientSearch} from '../../../infra/protocols/interfaces/query-ingredient-search.interface'
 import {APIGatewayProxyResult} from 'aws-lambda'
 import {IIngredientService} from '../../../domain/services/protocols/i-ingredient.service'
-import {noContent} from '../../../infra/http'
+import {noContent, ok} from '../../../infra/http'
 
 export class IngredientController implements IIngredientController {
 	constructor(private service: IIngredientService) {}
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	async search(params?: QueryIngredientSearch): Promise<APIGatewayProxyResult> {
-		return noContent()
+		const { data } = await this.service.search(params)
+
+		if (!data.results.length) {
+			return noContent()
+		}
+
+		return ok({})
 	}
 
 }
