bd453b976e89b0135f2e5387a263b4c25f363c00
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-20 15:45:37 -0300

    feat: create a invalid para error

--- src/infra/error/http/error.ts
+++ src/infra/error/http/error.ts
@@ -1,6 +1,7 @@
 import {InternalServerError} from '../internal-server.error'
 import {MissingParamError} from '../missing-param.error'
 import {BadRequestError} from '../bad-request.error'
+import {InvalidParamError} from '../invalid-param.error'
 
 export const internalServerError = () => {
 	const err = new InternalServerError()
@@ -22,6 +23,14 @@ export const badRequestError = () => {
 	const err = new BadRequestError()
 	return {
 		statusCode: 400,
+		body: JSON.stringify({ message: err.message })
+	}
+}
+
+export const invalidParamError = () => {
+	const err = new InvalidParamError()
+	return {
+		statusCode: 422,
 		body: JSON.stringify({ message: err.message })
 	}
 }
