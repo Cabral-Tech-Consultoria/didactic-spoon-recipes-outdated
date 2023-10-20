06ee9550992b1ba6a7cdc03c59293ff5266cb88e
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-20 07:56:53 -0300

    chore: add verification if "query" param is empty

--- src/presentation/controllers/ingredients/ingredient.controller.ts
+++ src/presentation/controllers/ingredients/ingredient.controller.ts
@@ -3,13 +3,17 @@ import {QueryIngredientSearch} from '../../../infra/protocols/interfaces/query-i
 import {APIGatewayProxyResult} from 'aws-lambda'
 import {IIngredientService} from '../../../domain/services/protocols/i-ingredient.service'
 import {noContent, ok} from '../../../infra/http'
-import {internalServerError} from '../../../infra/error/http/error'
+import {internalServerError, missingParamError} from '../../../infra/error/http/error'
 
 export class IngredientController implements IIngredientController {
 	constructor(private service: IIngredientService) {}
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	async search(params?: QueryIngredientSearch): Promise<APIGatewayProxyResult> {
 		try {
+			if (!params?.query) {
+				return missingParamError('query')
+			}
+
 			const { data } = await this.service.search(params)
 
 			if (!data.results.length) {
