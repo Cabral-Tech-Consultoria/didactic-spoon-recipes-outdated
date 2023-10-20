bfeaa8de8d06a90b52ac03e65d97a2de6a18eb63
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-20 09:41:33 -0300

    fix: fix dependency injection singleton

--- src/infra/dependency-injection/types.di.ts
+++ src/infra/dependency-injection/types.di.ts
@@ -3,13 +3,16 @@ import ServiceIdentifier = interfaces.ServiceIdentifier;
 import Newable = interfaces.Newable;
 
 export const TYPES_DI = {
-	RecipeService: Symbol.for('RecipeService')
+	RecipeService: Symbol.for('RecipeService'),
+	RecipeController: Symbol.for('RecipeController'),
+	IngredientService: Symbol.for('IngredientService'),
+	IngredientController: Symbol.for('IngredientController'),
 }
 
 export class DIContainerConfig {
 	static container: Container = new Container()
 
-	static bindClass<T>(type: ServiceIdentifier<T>) {
-		this.container.bind<T>(type).to(type as Newable<T>)
+	static bindClass<T>(symbol: symbol, type: ServiceIdentifier<T>) {
+		this.container.bind<T>(symbol).to(type as Newable<T>)
 	}
 }
