429172bab0e9687293a87593739d68f32dff67ce
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-21 13:38:40 -0300

    refactor: change the function to be more generic and scalable

--- src/main/validations/ingredients/index.ts
+++ src/main/validations/ingredients/index.ts
@@ -1,19 +1,15 @@
-import {
-	QueryComputeIngredientNutrientAmount
-} from '../../../infra/protocols/interfaces/query-compute-ingredient-nutrient-amount.interface'
 import {getProperty} from '../../../utils/objects/check-null-properties'
 
-export const validateComputeIngredientNutrientAmount = (params: QueryComputeIngredientNutrientAmount): string[] => {
-	const requiredProperties= ['nutrient', 'target']
+export const validateRequiredParams = <T extends object>(params: T, requiredParams: string[]): string[] => {
 	const propertiesMissing: string[] = []
 
 	Object
 		.keys(params)
 		.forEach((key) => {
-			if (requiredProperties.includes(key)) {
+			if (requiredParams.includes(key)) {
 				const propertyValue = getProperty(
 					params,
-          key as keyof QueryComputeIngredientNutrientAmount
+          key as keyof T
 				)
 
 				if (!propertyValue) {
