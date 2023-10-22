231511666fac5cb01ba06b3f1132edcb6d30a381
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-22 08:04:36 -0300

    refactor: turn parameters into optional

--- src/infra/protocols/interfaces/query-autocomplete-ingredient-search.interface.ts
+++ src/infra/protocols/interfaces/query-autocomplete-ingredient-search.interface.ts
@@ -1,7 +1,7 @@
 export interface QueryAutocompleteIngredientSearch {
   query: string
-  number: number
-  language: string
-  metaInformation: boolean
-  intolerances: string
+  number?: number
+  language?: string
+  metaInformation?: boolean
+  intolerances?: string
 }
