11ff68f711b7bbdc787eeb0f1959ad23864fabf9
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-22 09:11:39 -0300

    test: add test cases to ingredient substitutes controller

---
+++ test/presentation/controllers/ingredients/get-ingredient-substitutes.test.ts
@@ -0,0 +1,52 @@
+import 'reflect-metadata'
+import {describe, expect, jest, test} from '@jest/globals'
+import {makeSut} from '../../../test-domains/ingredients'
+import {badRequestError, internalServerError} from '../../../../src/infra/error/http/error'
+import {buildAxiosResponse, promiseResolver} from '../../../index'
+
+describe('Get Ingredient Substitutes Controller', () => {
+	//<editor-fold desc="Should return BadRequestError if no ingredientName was provided">
+	test('Should return BadRequestError if no ingredientName was provided', async () => {
+		const { controller } = makeSut()
+
+		const response = await controller.getIngredientSubstitutes()
+
+		expect(JSON.parse(response.body)).toEqual(JSON.parse(badRequestError().body))
+		expect(response.statusCode).toBe(badRequestError().statusCode)
+	})
+	//</editor-fold>
+
+	//<editor-fold desc="Should return BadRequestError if no data was returned">
+	test('Should return BadRequestError if no data was returned', async () => {
+		const { controller, service } = makeSut()
+
+		const mock = promiseResolver(buildAxiosResponse(null))()
+
+		jest
+			.spyOn(service, 'getIngredientSubstitutes')
+			.mockReturnValueOnce(mock)
+
+		const response = await controller.getIngredientSubstitutes({ ingredientName: 'chuck' })
+
+		expect(JSON.parse(response.body)).toEqual(JSON.parse(badRequestError().body))
+		expect(response.statusCode).toBe(badRequestError().statusCode)
+	})
+	//</editor-fold>
+
+	//<editor-fold desc="Should return InternalServerError if throws">
+	test('Should return InternalServerError if throws', async () => {
+		const { controller } = makeSut()
+
+		const mock = promiseResolver(internalServerError())()
+
+		jest
+			.spyOn(controller, 'getIngredientSubstitutes')
+			.mockReturnValueOnce(mock)
+
+		const response = await controller.getIngredientSubstitutes({ ingredientName: 'chuck' })
+
+		expect(JSON.parse(response.body)).toEqual(JSON.parse(internalServerError().body))
+		expect(response.statusCode).toBe(internalServerError().statusCode)
+	})
+	//</editor-fold>
+})
