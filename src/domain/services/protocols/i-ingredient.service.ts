6aba4dba2897fe4c25ea7575068bb1ee79fa8817
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-22 07:26:48 -0300

    chore: shortened imports

--- src/domain/services/protocols/i-ingredient.service.ts
+++ src/domain/services/protocols/i-ingredient.service.ts
@@ -1,17 +1,15 @@
-import {QueryIngredientSearch} from '../../../infra/protocols/interfaces/query-ingredient-search.interface'
 import {IIngredientSearchList} from '../../protocols/interfaces/ingredient-search.interface'
 import {AxiosResponse} from 'axios'
-import {QueryIngredientById} from '../../../infra/protocols/interfaces/query-ingredient-by-id.interface'
 import {IIngredientInfo} from '../../protocols/interfaces/ingredient.interface'
-import {
-	QueryComputeIngredientNutrientAmount
-} from '../../../infra/protocols/interfaces/query-compute-ingredient-nutrient-amount.interface'
 import {NutrientAmount} from '../../protocols/interfaces/nutrient-amount.interface'
-import {QueryConvertAmounts} from '../../../infra/protocols/interfaces/query-convert-amounts.interface'
 import {AmountConversion} from '../../protocols/interfaces/amount-conversion.interface'
 import {
-	QueryAutocompleteIngredientSearch
-} from '../../../infra/protocols/interfaces/query-autocomplete-ingredient-search.interface'
+	QueryAutocompleteIngredientSearch,
+	QueryIngredientSearch,
+	QueryIngredientById,
+	QueryComputeIngredientNutrientAmount,
+	QueryConvertAmounts
+} from '../../../infra/protocols/interfaces'
 
 export interface IIngredientService {
   search(params?: QueryIngredientSearch): Promise<AxiosResponse<IIngredientSearchList>>
