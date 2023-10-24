171df7f6c4121bcc85912535b813bdedfa310fb9
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-24 08:51:44 -0300

    chore: create a translation interface

--- src/domain/protocols/interfaces/translation.interface.ts
+++ src/domain/protocols/interfaces/translation.interface.ts
@@ -1,29 +1,3 @@
-export interface ITranslation {
-  translations: TranslatedTo[],
-  from: string,
-  protectedKeys: string[],
-  translatedCharacters: number
-}
-
-export interface TranslationRaw {
-  translations: TranslatedTo[],
-  from: string,
-  protected_keys: string[],
-  translated_characters: number
-}
-
-interface TranslatedTo {
-  to: string
-  translated: string[]
-}
-
-export const Translation = {
-	format: (raw: TranslationRaw): ITranslation => {
-		return {
-			translations: raw.translations,
-			from: raw.from,
-			protectedKeys: raw.protected_keys,
-			translatedCharacters: raw.translated_characters,
-		}
-	}
+export interface Translation<T> {
+  trans: T
 }
