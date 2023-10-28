5ffb120d60bd1ac4e21d740b256e4d6079881ad8
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-10-28 12:26:14 -0300

    chore: create a class translation text dto

--- src/infra/protocols/dtos/translation.dto.ts
+++ src/infra/protocols/dtos/translation.dto.ts
@@ -6,3 +6,12 @@ export class TranslationDTO<T> {
 		public json: T
 	) {}
 }
+
+export class TranslationTextDTO {
+	constructor(
+		public to: string,
+		public from: string,
+		public text: string
+	) {
+	}
+}
