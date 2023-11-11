39665f6f353a20b07427863497a6015a9ffc2869
Edson Cabral Junior <e2cabraljunior@gmail.com>
2023-11-11 17:36:11 -0300

    refactor: change to a separated auth provider

--- src/domain/services/auth.service.ts
+++ src/domain/services/auth.service.ts
@@ -1,23 +1,12 @@
 import {UserDTO} from '../../infra/protocols/dtos/user.dto'
 import {IAuthService} from './protocols/i-auth.service'
 import {injectable} from 'inversify'
-import {
-	CognitoIdentityProviderClient,
-	SignUpCommand,
-	SignUpCommandOutput
-} from '@aws-sdk/client-cognito-identity-provider'
+import {SignUpCommandOutput} from '@aws-sdk/client-cognito-identity-provider'
 import logger from '../../infra/logger/log'
+import {SignUp} from '../../infra/wrappers/auth.provider'
 
 @injectable()
 export class AuthService implements IAuthService {
-	private identityProvider: CognitoIdentityProviderClient
-
-	constructor() {
-		this.identityProvider = new CognitoIdentityProviderClient({
-			region: 'us-east-1',
-		})
-	}
-
 	async register(user: UserDTO): Promise<SignUpCommandOutput> {
 		logger.info({message: 'Param received', payload: user})
 
@@ -29,16 +18,14 @@ export class AuthService implements IAuthService {
 		attributes.push({Name: 'phone_number', Value: user.phoneNumber})
 
 		logger.info({message: 'Call the command to register the user'})
-		const commandInput = {
+		const command = {
 			ClientId: process.env.AWS_CLIENT_ID,
 			Username: user.username,
 			Password: user.password,
 			UserAttributes: attributes,
 		}
-		logger.info({message: 'Command input mounted', payload: commandInput})
-		const command = new SignUpCommand(commandInput)
 
 		logger.info({message: 'Send the command'})
-		return this.identityProvider.send(command)
+		return SignUp(command)
 	}
 }
